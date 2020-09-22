167d3a660d4fc3620eaffbfaa7ce153acd63e488
dhruv8780 <dhruvcanada01@gmail.com>
2020-09-22 16:56:57 -0400

    Code Updated

---
+++ Text101/Assets/Scripts/LifeStory.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class LifeStory : MonoBehaviour
+{
+    [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+
+    State state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = startingState;
+        textComponent.text = state.GetStateStory();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageState();
+    }
+
+    private void ManageState()
+    {
+        var nextStates = state.GetNextStates();
+        for(int index = 0; index < nextStates.Length; index++)
+
+
+        if (Input.GetKeyDown(KeyCode.Alpha1 + index))
+        {
+            state = nextStates[index];
+        }
+        /*else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            state = nextStates[1];
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            state = nextStates[2];
+        }*/
+        textComponent.text = state.GetStateStory();
+    }
+}
